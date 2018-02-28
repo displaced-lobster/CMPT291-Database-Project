4a6fd617daec268dbc953dfd826e29858c1390a0
Glenn-S <34552455+Glenn-S@users.noreply.github.com>
2018-02-27 18:25:25 -0700

    test commit, button moved again

--- Customer_Interface/Customer_Interface/Form1.Designer.vb
+++ Customer_Interface/Customer_Interface/Form1.Designer.vb
@@ -27,7 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(256, 70)
+        Me.Button1.Location = New System.Drawing.Point(423, 211)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
