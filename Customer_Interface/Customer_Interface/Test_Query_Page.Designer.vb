914974a22a79939a47df4446973519e8528417b7
Glenn-S <34552455+Glenn-S@users.noreply.github.com>
2018-03-14 21:56:41 -0600

    shit

--- Customer_Interface/Customer_Interface/Test_Query_Page.Designer.vb
+++ Customer_Interface/Customer_Interface/Test_Query_Page.Designer.vb
@@ -22,32 +22,32 @@ Partial Class Test_Query_Page
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Answer1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Answer1 = New System.Windows.Forms.Label()
         Me.exitBtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(96, 77)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Answer1
         '
         Me.Answer1.AutoSize = True
-        Me.Answer1.Location = New System.Drawing.Point(164, 67)
+        Me.Answer1.Location = New System.Drawing.Point(177, 82)
         Me.Answer1.Name = "Answer1"
-        Me.Answer1.Size = New System.Drawing.Size(33, 13)
-        Me.Answer1.TabIndex = 0
-        Me.Answer1.Text = "name"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(83, 62)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Execute"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Answer1.Size = New System.Drawing.Size(35, 13)
+        Me.Answer1.TabIndex = 1
+        Me.Answer1.Text = "Name"
         '
         'exitBtn
         '
-        Me.exitBtn.Location = New System.Drawing.Point(83, 144)
+        Me.exitBtn.Location = New System.Drawing.Point(96, 170)
         Me.exitBtn.Name = "exitBtn"
         Me.exitBtn.Size = New System.Drawing.Size(75, 23)
         Me.exitBtn.TabIndex = 2
@@ -58,10 +58,10 @@ Partial Class Test_Query_Page
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(511, 251)
+        Me.ClientSize = New System.Drawing.Size(480, 388)
         Me.Controls.Add(Me.exitBtn)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Answer1)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Test_Query_Page"
         Me.Text = "Test Query"
         Me.ResumeLayout(False)
@@ -69,7 +69,7 @@ Partial Class Test_Query_Page
 
     End Sub
 
-    Friend WithEvents Answer1 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents Answer1 As Label
     Friend WithEvents exitBtn As Button
 End Class
--- Customer_Interface/Customer_Interface/Test_Query_Page.vb
+++ Customer_Interface/Customer_Interface/Test_Query_Page.vb
@@ -2,12 +2,13 @@
 
 Public Class Test_Query_Page
     Private Shared Function GetConnectionString() As String
-        Return "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=DESKTOP-C77900C; Initial Catalog=CMPT291_Project; Integrated Security=True"
+        Return "Data Source=localhost\MSSQLSERVER2014;Initial Catalog=CMPT291_Project;Integrated Security=True"
 
     End Function
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
         'SQL QUERY
-        Dim connectionString As String = GetConnectionString()
+        Dim connectionString As String = "CMPT291_ProjectConnectionString"
 
         Dim queryString As String = "SELECT first_name FROM Customer_Data" ' + Idtxt.Text.ToString
 
@@ -19,23 +20,19 @@ Public Class Test_Query_Page
                 Dim dataReader As OleDbDataReader = command.ExecuteReader()
 
                 Do While dataReader.Read()
-                    Answer1.Text = dataReader(0).ToString
+                    Button1.Text = dataReader(0).ToString
                     'Answer2.Text = dataReader(1).ToString
                 Loop
-                Answer1.Refresh()
+                Button1.Refresh()
                 'Answer2.Refresh()
 
                 dataReader.Close()
 
             Catch ex As Exception
                 MsgBox(ex.Message)
-                ' Console.WriteLine(ex.Message)
+                'Console.WriteLine(ex.Message)
             End Try
         End Using
-    End Sub
-
-    Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
-        Me.Close()
     End Sub
 
 End Class
