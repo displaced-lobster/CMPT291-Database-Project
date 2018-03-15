4f80445d05d99318d1cb07532f7844c1185528b9
Glenn-S <34552455+Glenn-S@users.noreply.github.com>
2018-03-14 21:21:41 -0600

    Added DB string for connection

--- Customer_Interface/Customer_Interface/Test_Query_Page.Designer.vb
+++ Customer_Interface/Customer_Interface/Test_Query_Page.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Test_Query_Page
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,20 +20,21 @@ Partial Class Test_Query_Page
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Answer1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.exitBtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'Answer1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(164, 67)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(0, 13)
-        Me.Label1.TabIndex = 0
+        Me.Answer1.AutoSize = True
+        Me.Answer1.Location = New System.Drawing.Point(164, 67)
+        Me.Answer1.Name = "Answer1"
+        Me.Answer1.Size = New System.Drawing.Size(33, 13)
+        Me.Answer1.TabIndex = 0
+        Me.Answer1.Text = "name"
         '
         'Button1
         '
@@ -60,7 +61,7 @@ Partial Class Test_Query_Page
         Me.ClientSize = New System.Drawing.Size(511, 251)
         Me.Controls.Add(Me.exitBtn)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Answer1)
         Me.Name = "Test_Query_Page"
         Me.Text = "Test Query"
         Me.ResumeLayout(False)
@@ -68,7 +69,7 @@ Partial Class Test_Query_Page
 
     End Sub
 
-    Friend WithEvents Label1 As Label
+    Friend WithEvents Answer1 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents exitBtn As Button
 End Class
--- Customer_Interface/Customer_Interface/Test_Query_Page.vb
+++ Customer_Interface/Customer_Interface/Test_Query_Page.vb
@@ -1,12 +1,41 @@
-﻿Public Class Test_Query_Page
+﻿Imports System.Data.OleDb
+
+Public Class Test_Query_Page
+    Private Shared Function GetConnectionString() As String
+        Return "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=DESKTOP-C77900C; Initial Catalog=CMPT291_Project; Integrated Security=True"
+
+    End Function
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'SQL QUERY
+        Dim connectionString As String = GetConnectionString()
 
+        Dim queryString As String = "SELECT first_name FROM Customer_Data" ' + Idtxt.Text.ToString
 
-        'End
+        Using connection As New OleDbConnection(connectionString)
+            Dim command As OleDbCommand = connection.CreateCommand()
+            command.CommandText = queryString
+            Try
+                connection.Open()
+                Dim dataReader As OleDbDataReader = command.ExecuteReader()
+
+                Do While dataReader.Read()
+                    Answer1.Text = dataReader(0).ToString
+                    'Answer2.Text = dataReader(1).ToString
+                Loop
+                Answer1.Refresh()
+                'Answer2.Refresh()
+
+                dataReader.Close()
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+                ' Console.WriteLine(ex.Message)
+            End Try
+        End Using
     End Sub
 
     Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
         Me.Close()
     End Sub
+
 End Class
