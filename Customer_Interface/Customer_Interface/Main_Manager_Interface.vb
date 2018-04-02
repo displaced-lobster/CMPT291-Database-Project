eac207d918220a73b2f9087eb2f52a0699ab68c5
Bruce <brucedzss@gmail.com>
2018-04-02 11:59:29 -0600

    Fixed a small error.

--- Customer_Interface/Customer_Interface/Main_Manager_Interface.vb
+++ Customer_Interface/Customer_Interface/Main_Manager_Interface.vb
@@ -61,10 +61,6 @@
 
     End Sub
 
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles MI_add_emp_btn.Click
-
-    End Sub
-
     Private Sub MI_list_movies_btn_Click(sender As Object, e As EventArgs) Handles MI_list_movies_btn.Click
         List_All_Movies.Show()
     End Sub
@@ -112,6 +108,13 @@
                          MI_movie_type_tb.Text & "', " & MI_dist_fee_tb.Text & ", " & MI_inventory_tb.Text & ", 0);")
     End Sub
 
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles MI_add_emp_btn.Click
+        SQL.ExecuteQuery("INSERT INTO Employee_Data (SIN, first_name, last_name, city, state, zip_code, street, street_num, apartment_num, start_date, hourly_rate) " &
+                         "VALUES (" & MI_emp_sin_tb.Text & ", '" & MI_emp_fn_tb.Text & "', '" & MI_emp_ln_tb.Text & "', '" & MI_emp_city_tb.Text &
+                                  "', '" & MI_emp_state_tb.Text & "', " & MI_emp_zip_tb.Text & ", '" & MI_emp_street_tb.Text & "', " & MI_emp_street_num_tb.Text &
+                                  ", " & MI_emp_ap_num_tb.Text & ", '" & MI_emp_start_tb.Text & "', " & MI_emp_hr_tb.Text & ");")
+    End Sub
+
     Private Sub MI_edit_movie_btn_Click(sender As Object, e As EventArgs) Handles MI_edit_movie_btn.Click
         SQL.ExecuteQuery("UPDATE Movie_Data " &
                          "SET movie_name = '" & MI_movie_title_tb.Text & "', movie_type = '" & MI_movie_type_tb.Text &
@@ -127,5 +130,28 @@
         MI_movie_type_tb.Text = ""
         MI_dist_fee_tb.Text = ""
         MI_inventory_tb.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles emp_search_btn.Click
+        If (MI_movie_id_tb.Text.Length > 0) Then
+            SQL.ExecuteQuery("Select * From Employee_Data Where SIN = " & MI_emp_sin_tb.Text & ";")
+
+            If SQL.HasException(True) Then Exit Sub
+
+            Try
+                MI_emp_fn_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("first_name"))
+                MI_emp_ln_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("last_name"))
+                MI_emp_city_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("city"))
+                MI_emp_state_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("state"))
+                MI_emp_zip_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("zip_code"))
+                MI_emp_street_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("street"))
+                MI_emp_street_num_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("steet_num"))
+                MI_emp_ap_num_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("apartment_num"))
+                MI_emp_start_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("start_date"))
+                MI_emp_hr_tb.Text = Convert.ToString(SQL.SQLTable.Rows(0).Item("hourly_rate"))
+            Catch
+                Exit Sub
+            End Try
+        End If
     End Sub
 End Class
