4a3146380eaa3a35ffd760e77fa7d952f4992095
Glenn-S <34552455+Glenn-S@users.noreply.github.com>
2018-04-06 02:35:14 -0600

    fixed weird error glitch of ms VS

--- Customer_Interface/Customer_Interface/ManagerHelp.vb
+++ Customer_Interface/Customer_Interface/ManagerHelp.vb
@@ -3,18 +3,6 @@
         MdiParent = Main_Interface
     End Sub
 
-    Private Sub GroupBox9_Enter(sender As Object, e As EventArgs) Handles GroupBox9.Enter
-
-    End Sub
-
-    Private Sub Label10_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
-
-    End Sub
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.Hide()
         Main_Interface.pbBlueBox.Show()
